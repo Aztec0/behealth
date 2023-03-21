c03ec42ab04e171dd561f151ac28b06eb1a2fecf
Aztec0 <atupota17@gmail.com>
2023-03-21 23:13:38 +0200

    minor corrections

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::SessionsController < ApplicationController
   before_action :user
   def create
-    @user = user&.find_by(email: params[:email])
-    if @user && @user&.authenticate(params[:password])
+    @user = user.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
       # Generate JWT token for patient
       token = JWT.encode({ user_id: @user.id, type: params[:user_type]}, Rails.application.secret_key_base)
       render json: { token: token }
