a05134bd7d020e80aa7986b4ef2220525f521d8b
Aztec0 <atupota17@gmail.com>
2023-03-21 23:05:31 +0200

    refactor auth

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,22 +1,18 @@
 class Api::V1::SessionsController < ApplicationController
-
+  before_action :user
   def create
-
-    doctor = user.find_by(email: params[:email])
-    if patient && patient.authenticate(params[:password])
+    @user = user&.find_by(email: params[:email])
+    if @user && @user&.authenticate(params[:password])
       # Generate JWT token for patient
-      token = JWT.encode({ patient_id: patient.id, type: "patient" }, Rails.application.secret_key_base)
-      render json: { token: token }
-    elsif doctor && doctor.authenticate(params[:password])
-      # Generate JWT token for doctor
-      token = JWT.encode({ doctor_id: doctor.id, type: "doctor" }, Rails.application.secret_key_base)
+      token = JWT.encode({ user_id: @user.id, type: params[:user_type]}, Rails.application.secret_key_base)
       render json: { token: token }
     else
       render json: { error: 'Invalid credentials' }, status: :unauthorized
     end
   end
 
+  private
   def user
-    params[:user_type].constantize
+    params[:user_type].capitalize.constantize
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,49 @@
 class ApplicationController < ActionController::API
+  # before_action :authenticate_request
+
+  private
+
   def authenticate_request
+    token = extract_token_from_header
+    return render_error('Missing token', :unauthorized) unless token
+
+    decoded_token = decode_token(token)
+    return render_error('Invalid token', :unauthorized) unless decoded_token
+
+    user = fetch_user_from_database(decoded_token)
+    return render_error('Invalid token', :unauthorized) unless user
+
+    set_current_user(user)
+  end
+
+  def extract_token_from_header
     auth_header = request.headers['Authorization']
-    if auth_header && auth_header.split(' ').last
-      token = auth_header.split(' ').last
-      begin
-        decoded_token = JWT.decode(token, Rails.application.secret_key_base)
-        user_id = decoded_token[0][params[:user_type]]
-        if decoded_token[0]['type'] == "patient"
-          if Patient.exists?(user_id)
-            @current_patient = Patient.find(user_id)
-          else
-            render json: { error: 'Invalid token' }, status: :unauthorized
-          end
-        elsif decoded_token[0]['type'] == "doctor"
-          if Doctor.exists?(user_id)
-            @current_doctor = Doctor.find(user_id)
-          else
-            render json: { error: 'Invalid token' }, status: :unauthorized
-          end
-        else
-          render json: { error: 'Invalid token type' }, status: :unauthorized
-        end
-      rescue JWT::DecodeError
-        render json: { error: 'Invalid token' }, status: :unauthorized
-      end
-    else
-      render json: { error: 'Missing token' }, status: :unauthorized
+    auth_header&.split(' ')&.last
+  end
+
+  def decode_token(token)
+    JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256').first
+  rescue JWT::DecodeError
+    nil
+  end
+
+  def fetch_user_from_database(decoded_token)
+    type = decoded_token['type']
+    user_id = decoded_token['user_id']
+    return nil unless %w[patient doctor].include?(type)
+
+    type.capitalize.constantize.find_by(id: user_id)
+  end
+
+  def set_current_user(user)
+    if user.is_a?(Patient)
+      @current_patient = user
+    elsif user.is_a?(Doctor)
+      @current_doctor = user
     end
+  end
+
+  def render_error(message, status)
+    render json: { error: message }, status: status
   end
 end
