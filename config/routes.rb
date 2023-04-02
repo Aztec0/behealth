5c5e40b39d30d0ef4cac6e9b3b25af58487427b7
ShevchukIhor <bluesjazz666@gmail.com>
2023-04-02 21:27:51 +0300

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,16 @@ Rails.application.routes.draw do
       post '/login', to: 'sessions#create'
       post '/forgot', to: 'passwords#forgot'
       post 'password/reset', to: 'password#reset'
-      resources :head_doctors do
-        #get 'canceled_appointments', on: :collection
-        get 'index(/:sort_by_created_at)(/:sort_by_name)(/:sort_by_position)', action: :index, on: :collection
+      resources :head_doctors, only: [:index] do
+        collection do
+          get :canceled_appointments
+          post :create_doctor
+          post :create_hospital
+        end
+
+        member do
+          delete :delete_doctor
+        end
       end
     end
   end
