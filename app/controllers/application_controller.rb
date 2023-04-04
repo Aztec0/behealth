7dcdce88bf10403591fbce7211141412ec11056f
ShevchukIhor <bluesjazz666@gmail.com>
2023-04-04 14:30:47 +0300

    edit include Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  include Pundit::Authorization
+  include Pundit
   # before_action :authenticate_request
 
   def temporary_password
