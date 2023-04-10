f3680f642fe710f1cbe05e6498a02cc642ff4921
ShevchukIhor <bluesjazz666@gmail.com>
2023-04-10 14:45:27 +0300

    refactoring role in doctor

--- db/migrate/20230403050823_add_role_to_doctor.rb
+++ db/migrate/20230403050823_add_role_to_doctor.rb
@@ -1,5 +1,5 @@
 class AddRoleToDoctor < ActiveRecord::Migration[7.0]
   def change
-    add_column :doctors, :role, :integer, default: 1
+    add_column :doctors, :role, :integer, default: 0
   end
 end
