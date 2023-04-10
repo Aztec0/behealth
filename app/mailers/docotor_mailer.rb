c38aa9c31c82e3c6c67584aced203073fe649f52
ShevchukIhor <bluesjazz666@gmail.com>
2023-04-10 14:50:03 +0300

    add method for send temp_password to doctor when it create

--- app/mailers/docotor_mailer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class DoctorMailer < ApplicationMailer
-  def temporary_password(doctor)
-    @doctor = doctor
-    mail(to: doctor.email, subject: 'Your temporary password')
-  end
-end
