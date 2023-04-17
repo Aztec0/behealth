2bdd87795e8c2b4db4bffc558730b467b8ec3790
ShevchukIhor <bluesjazz666@gmail.com>
2023-04-17 08:16:57 +0300

    minor refactoring

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -3,10 +3,10 @@
 class Api::V1::SearchController < ApplicationController
   before_action :force_json
   def search
-    @hospitals = Hospital.joins(:doctors).ransack(address_or_city_or_name_or_region_cont: params[:query],
-                                                  region_cont: params[:region]).result(distinct: true).limit(5)
-    @doctors = Doctor.joins(:hospital).ransack(name_or_surname_or_position_cont: params[:query],
-                                               region_cont: params[:region]).result(distinct: true).limit(5)
+    @hospitals = Hospital.joins(:doctors).ransack(address_or_city_or_name_or_region_i_cont: params[:query],
+                                                  region_i_cont: params[:region]).result(distinct: true).limit(5)
+    @doctors = Doctor.joins(:hospital).ransack(name_or_surname_or_position_i_cont: params[:query],
+                                               region_i_cont: params[:region]).result(distinct: true).limit(5)
     # render json: { hospitals: hospitals, doctors: doctors }, status: :ok, each_serializer: Api::V1::SearchSerializer
   end
 
