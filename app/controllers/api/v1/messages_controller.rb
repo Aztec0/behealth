d7d50a5f95506fd46d469b9477cde2ff93f967b8
Eiserich <koroviakovskiy@ukr.net>
2023-05-05 22:24:41 +0300

    update spec for messages

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,30 +1,26 @@
 class Api::V1::MessagesController < ApplicationController
   before_action :set_chat
   before_action :set_message, only: [:show, :update, :destroy]
-  # GET /chats/:chat_id/messages
+
   def index
-    chat = current_user.chats.find(params[:chat_id])
-    render json: chat.messages
+    render json: @chat.messages
   end
 
   def show
     render json: @message
   end
 
-  # POST /chats/:chat_id/messages
   def create
-    chat = current_user.chats.find(params[:chat_id])
-    message = chat.messages.build(message_params)
-    message.user = current_user
+    @message = @chat.messages.build(message_params)
+    @message.user = current_user
 
-    if message.save
-      render json: message, status: :created
+    if @message.save
+      render json: @message, status: :created
     else
-      render json: message.errors.full_messages, status: :unprocessable_entity
+      render json: @message.errors.full_messages, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /chats/:chat_id/messages/:id
   def update
     if @message.update(message_params)
       render json: @message
@@ -33,7 +29,6 @@ class Api::V1::MessagesController < ApplicationController
     end
   end
 
-  # DELETE /chats/:chat_id/messages/:id
   def destroy
     @message.destroy
     head :no_content
