8b3f31d9a982be9dabad6f2092ebe7e42e70678b
Aztec0 <atupota17@gmail.com>
2023-05-06 01:14:23 +0300

    fixed errors

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_095801) do
   end
 
   create_table "calendars", force: :cascade do |t|
+    t.string "name"
     t.bigint "doctor_id", null: false
     t.bigint "patient_id", null: false
     t.datetime "created_at", null: false
