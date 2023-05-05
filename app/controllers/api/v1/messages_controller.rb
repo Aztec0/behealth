6c1bb99a3a3ee75b4d40b449e2f8daa10878a9a7
Eiserich <koroviakovskiy@ukr.net>
2023-05-05 20:59:09 +0300

    minor update for messages

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::MessagesController < ApplicationController
-
+  before_action :set_chat
+  before_action :set_message, only: [:show, :update, :destroy]
   # GET /chats/:chat_id/messages
   def index
     chat = current_user.chats.find(params[:chat_id])
     render json: chat.messages
+  end
+
+  def show
+    render json: @message
   end
 
   # POST /chats/:chat_id/messages
@@ -19,9 +24,31 @@ class Api::V1::MessagesController < ApplicationController
     end
   end
 
+  # PATCH/PUT /chats/:chat_id/messages/:id
+  def update
+    if @message.update(message_params)
+      render json: @message
+    else
+      render json: @message.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /chats/:chat_id/messages/:id
+  def destroy
+    @message.destroy
+    head :no_content
+  end
+
   private
 
-  # Only allow a list of trusted parameters through.
+  def set_chat
+    @chat = current_user.chats.find(params[:chat_id])
+  end
+
+  def set_message
+    @message = @chat.messages.find(params[:id])
+  end
+
   def message_params
     params.require(:message).permit(:content)
   end
