fde54eed2fead8d652ceb1e6ad4e5bdb02dbe630
Eiserich <koroviakovskiy@ukr.net>
2023-05-05 14:35:25 +0300

    minor update

--- spec/requests/api/v1/calendars_spec.rb
+++ spec/requests/api/v1/calendars_spec.rb
@@ -61,23 +61,20 @@ RSpec.describe 'api/v1/calendars', swagger_doc: 'v1/swagger.yaml', type: :reques
 
     run_test!
   end
-end
-
     path '/api/v1/calendars/{id}' do
       patch('Update a calendar') do
         tags 'Calendars'
         security [{ ApiKeyAuth: [] }]
         consumes 'application/json'
+        parameter name: :id, in: :path, type: :string
         parameter name: :calendar_params, in: :body, schema: {
           type: :object,
           properties: {
             start_time: { type: :string, default: '2023-05-05 10:00:00' },
             end_time: { type: :string, default: '2023-05-05 11:00:00' },
-            note: { type: :string, default: 'Updated note' },
           },
-          required: %w[start_time end_time note]
+          required: %w[start_time end_time]
         }
-        parameter name: :id, in: :path, type: :string
 
         response '200', 'returns the updated calendar' do
           schema type: :object,
@@ -85,43 +82,39 @@ end
                    id: { type: :integer },
                    start_time: { type: :string },
                    end_time: { type: :string },
-                   note: { type: :string },
-                   user_id: { type: :integer }
+                   current_user_id: { type: :integer }
                  },
-                 required: %w[id start_time end_time note user_id]
+                 required: %w[id start_time end_time current_user_id]
 
+          let(:id) { create(:calendar).id }
           let(:calendar_params) do
             {
-              start_time: '2023-05-05 10:00:00',
-              end_time: '2023-05-05 11:00:00',
-              note: 'Updated note'
+              start_time: '2023-05-05 12:00:00',
+              end_time: '2023-05-05 13:00:00'
             }
           end
 
-          run_test! do
-            expect(json['note']).to eq('Updated note')
-          end
+          run_test!
         end
 
         response '404', 'calendar not found' do
           let(:id) { 'invalid' }
           let(:calendar_params) do
             {
-              start_time: '2023-05-05 10:00:00',
-              end_time: '2023-05-05 11:00:00',
-              note: 'Updated note'
+              start_time: '2023-05-05 12:00:00',
+              end_time: '2023-05-05 13:00:00'
             }
           end
+
           run_test!
         end
 
         response '401', 'unauthorized' do
-          let(:id) { calendar.id }
+          let(:id) { create(:calendar).id }
           let(:calendar_params) do
             {
-              start_time: '2023-05-05 10:00:00',
-              end_time: '2023-05-05 11:00:00',
-              note: 'Updated note'
+              start_time: '2023-05-05 12:00:00',
+              end_time: '2023-05-05 13:00:00'
             }
           end
 
